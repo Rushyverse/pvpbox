df64af84519febfd30d776188aa6283a225c6142
Cizetux <cizetux@gmail.com>
2023-03-20 21:17:32 +0100

    chore: Update scoreboard

--- src/main/kotlin/com/github/rushyverse/pvpbox/scoreboard/PvpboxScoreboard.kt
+++ src/main/kotlin/com/github/rushyverse/pvpbox/scoreboard/PvpboxScoreboard.kt
@@ -1,48 +1,89 @@
 package com.github.rushyverse.pvpbox.scoreboard
 
+import com.github.rushyverse.api.extension.undefineBold
 import com.github.rushyverse.api.extension.withBold
 import com.github.rushyverse.api.extension.withItalic
 import com.github.rushyverse.api.translation.SupportedLanguage
 import com.github.rushyverse.api.translation.TranslationsProvider
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
 import net.minestom.server.entity.Player
 import net.minestom.server.scoreboard.Sidebar
 
 class PvpboxScoreboard(val translationsProvider: TranslationsProvider, val player: Player) :
-    Sidebar(Component.text("   PvPBox   ",NamedTextColor.RED).withBold().withItalic()) {
+    Sidebar(Component.text("PvPBox", TextColor.fromHexString("#e500e8")).withBold()) {
 
     init {
         val playerLocale = SupportedLanguage.ENGLISH.locale
 
-        createLine(ScoreboardLine("emptyLine3", Component.empty(), 5))
+        createLine(ScoreboardLine("emptyLine4", Component.empty(), 11))
+
+        createLine(
+            ScoreboardLine(
+                "level",
+                Component.text("Level:", NamedTextColor.WHITE)
+                    .append(Component.text(" 0" + "✩", NamedTextColor.GRAY)),
+                10
+            )
+        )
+
+        createLine(ScoreboardLine("emptyLine3", Component.empty(), 9))
+
+        createLine(
+            ScoreboardLine(
+                "dust",
+                Component.text("Dust:", NamedTextColor.WHITE)
+                    .append(Component.text(" 0" + "✵", NamedTextColor.AQUA)),
+                8
+            )
+        )
+
+        createLine(
+            ScoreboardLine(
+                "status",
+                Component.text("Status:", NamedTextColor.WHITE)
+                    .append(Component.text(" Fighting", NamedTextColor.RED)),
+                7
+            )
+        )
+
+        createLine(ScoreboardLine("emptyLine2", Component.empty(), 6))
 
         createLine(
             ScoreboardLine(
                 "kills",
                 Component.text("Kills:", NamedTextColor.WHITE)
-                    .append(Component.text(" 0", NamedTextColor.AQUA)),
-                4
+                    .append(Component.text(" 0", NamedTextColor.GRAY)),
+                5
             )
         )
+
         createLine(
             ScoreboardLine(
                 "deaths",
                 Component.text("Deaths:", NamedTextColor.WHITE)
-                    .append(Component.text(" 0", NamedTextColor.RED)),
-                2
+                    .append(Component.text(" 0", NamedTextColor.GRAY)),
+                4
             )
         )
 
-        createLine(ScoreboardLine("emptyLine2", Component.empty(), 3))
+        createLine(
+            ScoreboardLine(
+                "killstreak",
+                Component.text("Killstreak:", NamedTextColor.WHITE)
+                    .append(Component.text(" 0", NamedTextColor.GRAY)),
+                3
+            )
+        )
 
-        createLine(ScoreboardLine("emptyLine1", Component.empty(), 1))
+        createLine(ScoreboardLine("emptyLine1", Component.empty(), 2))
 
         createLine(
             ScoreboardLine(
                 "ip",
-                Component.text("  rushy.space", NamedTextColor.LIGHT_PURPLE).withItalic(),
-                0
+                Component.text(" www.rushy.space ", TextColor.fromHexString("#e500e8")).withItalic(),
+                1
             )
         )
     }
